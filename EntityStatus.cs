bac1e94396a99f7826d2661240a57d89cebcbafd
kuritaro1122 <62167170+kuritaro1122@users.noreply.github.com>
2021-09-05 11:28:56 +0900

    Add files via upload

---
+++ EntityStatus.cs
@@ -0,0 +1,100 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using CriWare;
+
+namespace EntityBehavior.Status {
+    [AddComponentMenu("EntityBehavior/EntityStatus")]
+    public /*abstract*/ class EntityStatus : MonoBehaviour {
+        public enum EntityType { enemy, friend, obstacle }
+        private static readonly string commonTag = "Entity";
+
+        [Header("--- Entity Status ---")]
+        [SerializeField] protected EntityType entityType;
+        [SerializeField] protected bool isShot = false; //shot同士は属性に関わらず衝突しない。
+        [SerializeField] bool takeNoDamage = false; //Defeat()でのみ破壊可能になる。
+        [SerializeField, Min(0f)] float MaxHP = 20f;
+        [SerializeField, Min(0f)] float HP = 20f; //shotの時は値の数だけ貫通する。
+        [SerializeField, Min(0f)] float power = 5f;
+
+        //[SerializeField] UnityEvent damagedEvents;
+        //[SerializeField] UnityEvent defeatedEvents;
+
+        public EntityType Type { get { return this.entityType; } }
+        public float Power { get { return this.power; } }
+        public float GetHP() => this.HP;
+        public float GetMaxHP() => this.MaxHP;
+
+        public void Set(float? HP, float? power, EntityType? entityType = null, bool? isShot = null, bool? takeNoDamage = null) {
+            this.HP = HP ?? this.HP;
+            this.power = power ?? this.power;
+            this.entityType = entityType ?? this.entityType;
+            this.isShot = isShot ?? this.isShot;
+            this.takeNoDamage = takeNoDamage ?? this.takeNoDamage;
+        }
+
+        protected virtual void Awake() {
+            HP = MaxHP;
+            this.tag = commonTag;
+            defeat = false;
+        }
+        protected virtual void OnValidate() {
+            ClampHP();
+        }
+        protected virtual void OnTriggerEnter(Collider other) {
+            CollisionConfirmation(other);
+        }
+        protected virtual void OnCollisionEnter(Collision collision) {
+            CollisionConfirmation(collision.collider);
+        }
+
+        private void CollisionConfirmation(Collider other) {
+            if (other.gameObject.tag == commonTag) {
+                EntityStatus otherStatus = other.gameObject.GetComponent<EntityStatus>();
+                if (otherStatus == null) return;
+                bool differentType = (this.Type != otherStatus.Type);
+                bool bothAreShot = this.isShot && otherStatus.isShot;
+                if (differentType && !bothAreShot) OtherEntityCollision(otherStatus);
+            }
+        }
+        private void OtherEntityCollision(EntityStatus otherStatus) {
+            int id = this.gameObject.GetInstanceID();
+            int otherId = otherStatus.gameObject.GetInstanceID();
+            if (id < otherId) {
+                float otherPower = otherStatus.Power;
+                if (this.Power > 0f) otherStatus.Damage(this.Power);
+                if (otherPower > 0f) this.Damage(otherPower);
+            }
+        }
+        private bool defeat = false;
+        public virtual void Damage(float value = 1) {
+            if (value <= 0f) {
+                Debug.LogWarningFormat("Damage value <= 0f", this);
+                return;
+            }
+            if (takeNoDamage) return;
+            HP -= !isShot ? value : 1f; //shotの時はダメージが1固定
+            //damagedEvents.Invoke();
+            if (this.HP <= 0f) {
+                if (defeat == false) {
+                    defeat = true;
+                    Defeat();
+                } else Debug.LogWarning("EntityStatus/already defeat.");
+            }
+        }
+        public virtual void Recover(float value) {
+            HP += !isShot ? value : 1f;
+            ClampHP();
+        }
+        public virtual void Defeat() {
+            //defeatedEvents.Invoke();
+            Destroy(this.gameObject);
+        }
+
+        private void ClampHP() {
+            HP = Mathf.Clamp(HP, 0f, MaxHP);
+            if (MaxHP <= 0f) Debug.LogWarningFormat("MaxHP <= 0f", this);
+        }
+    }
+}
---
+++ EntityStatus_hyper.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EntityBehavior.Status;
+using HyperNova.Game;
+
+namespace HyperNova.Entity {
+    [AddComponentMenu("HyperNova/Entity/Entity_Status")]
+    public class EntityStatus_hyper : EntityStatus {
+        [Header("=== Entity Status ===")]
+        [SerializeField, Min(0)] protected int score;
+        [SerializeField] bool ignoreScoreLimit = false;
+        [SerializeField] bool tough = false;
+        [Header("--- Effect ---")]
+        [SerializeField] protected GameObject eDestroyEffect;
+        [Header("--- Drop Item ---")]
+        [SerializeField] protected GameObject dropItem;
+        //[Header("--- SE ---")]
+        //[SerializeField, Min(0)] protected int eHitSE_ID = 4;
+        //[SerializeField, Min(0)] protected int eDestroySE_ID = 3;
+
+        protected override void OnValidate() {
+            base.OnValidate();
+        }
+
+        //引数がnullでも良いように
+        public EntityStatus_hyper Set(float? HP = null, float? power = null, int? score = null, GameObject dropItem = null, bool? isShot = null, bool? takeNoDamage = null) {
+            base.Set(HP, power, EntityType.enemy, isShot, takeNoDamage);
+            this.score = score ?? this.score;
+            this.dropItem = dropItem;
+            return this;
+        }
+
+        public EntityStatus_hyper Set(float? HP = null, float? power = null, int? score = null) {
+            base.Set(HP, power, EntityType.enemy, isShot);
+            this.score = score ?? this.score;
+            return this;
+        }
+
+
+        public EntityStatus_hyper Set(GameObject dropItem = null) {
+            this.dropItem = dropItem;
+            return this;
+        }
+
+        public override void Damage(float value = 1) {
+            base.Damage(value);
+            SoundManager.enemyHitSESource.Play();
+        }
+
+        public override void Defeat() {
+            SoundManager.enemyDestroySESource.Play();
+            GameManager.Instance.ScoreAddition(score, ignoreScoreLimit);
+            InstantiateDestroyEffect();
+            InstantiateDropItem();
+            base.Defeat();
+        }
+
+        public void CollisionWithPlayer() {
+            InstantiateDestroyEffect();
+            if (!tough) {
+                SoundManager.enemyDestroySESource.Play();
+                Destroy(this.gameObject);
+            }
+        }
+
+        private void InstantiateDestroyEffect(Vector3 relativePos) {
+            if (eDestroyEffect != null)
+                Instantiate(eDestroyEffect, transform.position + relativePos, Quaternion.identity);
+        }
+        private void InstantiateDestroyEffect() => InstantiateDestroyEffect(Vector3.zero);
+        private void InstantiateDropItem() {
+            if (dropItem != null)
+                Instantiate(dropItem, transform.position, Quaternion.identity);
+        }
+    }
+}
