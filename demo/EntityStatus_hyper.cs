53118146f539984b7fef297649037382412914f9
くり太郎 <62167170+kuritaro1122@users.noreply.github.com>
2022-04-08 17:35:23 +0900

    Update EntityStatus_hyper.cs

--- demo/EntityStatus_hyper.cs
+++ demo/EntityStatus_hyper.cs
@@ -1,16 +1,16 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using EntityBehavior.Status;
-using HyperNova.Game;
 using System;
 
 namespace HyperNova.Entity {
-    [AddComponentMenu("HyperNova/Entity/Entity_Status")]
+    [AddComponentMenu("HyperNova/Entity/EntityStatus_hyper")]
     public class EntityStatus_hyper : EntityStatus {
         [Header("=== Entity Status ===")]
         [SerializeField, Min(0)] protected int score;
+        public int Score => this.score;
         [SerializeField] bool ignoreScoreLimit = false;
+        [SerializeField] bool ignoreScoreCombo = false;
         [SerializeField] bool tough = false;
         [Header("--- OffSet ---")]
         [SerializeField] Vector3 offSet = Vector3.zero;
@@ -24,7 +24,6 @@ namespace HyperNova.Entity {
         [SerializeField] ObjectSelectType selectType;
         private enum ObjectSelectType { @Object, ObjectFunc }
         [SerializeField] protected GameObject dropItem;
-        //[SerializeField] Vector3 itemOffset = Vector3.zero;
         private Func<GameObject> dropItemFunc = () => { return null; };
         private Coroutine colorEffectCoroutine = null;
         [Header("--- Out of Screen ---")]
@@ -34,7 +33,6 @@ namespace HyperNova.Entity {
         [SerializeField] bool destroyOverUp = false;
         [SerializeField] bool destroyOverDown = false;
         [SerializeField] Vector2 excess = new Vector2(0, 0);
-
         private struct ColorChanger {
             [SerializeField] Material material;
             [SerializeField] Color defaultColor;
@@ -46,10 +44,12 @@ namespace HyperNova.Entity {
             public void AddColor(Color color) => this.material.color += color;
             public void ResetColor() => this.material.color = defaultColor;
         }
+        private ColorChanger[] colorChangers = null;
 
         protected override void Awake() {
             base.Awake();
             if (this.cam == null) this.cam = Camera.main;
+            InitColorChanger();
         }
         protected virtual void Update() {
             Limit();
@@ -66,20 +66,22 @@ namespace HyperNova.Entity {
         protected override void OnValidate() {
             base.OnValidate();
         }
+        protected virtual void OnDrawGizmos() {
+            Gizmos.DrawWireSphere(this.transform.TransformPoint(offSet), 1f);
+        }
 
-        public EntityStatus_hyper Set(float? HP = null, float? power = null, int? score = null, GameObject dropItem = null, bool? isShot = null, bool? takeNoDamage = null) {
-            base.Set(HP, power, EntityType.enemy, isShot, takeNoDamage);
+        public EntityStatus_hyper Set(float? HP = null, float? power = null, int? score = null, GameObject dropItem = null, bool? isShot = null, DamageType? damageType = null) {
+            base.Set(HP, power, EntityType.enemy, isShot, damageType);
             this.score = score ?? this.score;
             Set(dropItem);
             return this;
         }
-        public EntityStatus_hyper Set(float? HP = null, float? power = null, int? score = null, Func<GameObject> dropItemFunc = null, bool? isShot = null, bool? takeNoDamage = null) {
-            base.Set(HP, power, EntityType.enemy, isShot, takeNoDamage);
+        public EntityStatus_hyper Set(float? HP = null, float? power = null, int? score = null, Func<GameObject> dropItemFunc = null, bool? isShot = null, DamageType? damageType = null) {
+            base.Set(HP, power, EntityType.enemy, isShot, damageType);
             this.score = score ?? this.score;
             Set(dropItemFunc);
             return this;
         }
-
         public EntityStatus_hyper Set(float? HP = null, float? power = null, int? score = null) {
             base.Set(HP, power, EntityType.enemy, isShot);
             this.score = score ?? this.score;
@@ -95,7 +97,10 @@ namespace HyperNova.Entity {
             this.dropItemFunc = dropItemFunc;
             return this;
         }
-
+        public EntityStatus_hyper Set(int score) {
+            this.score = score;
+            return this;
+        }
         public EntityStatus_hyper Set(bool destroyOverLeft, bool destroyOverRight, bool destroyOverUp, bool destroyOverDown) {
             this.destroyOverLeft = destroyOverLeft;
             this.destroyOverRight = destroyOverRight;
@@ -104,39 +109,41 @@ namespace HyperNova.Entity {
             return this;
         }
 
-        public override void Damage(float value = 1) {
-            if (!base.takeNoDamage) {
-                base.Damage(value);
-                ADX2LEManager.Instance.PlaySE("EnemyHit");
+        public override void Damage(float value = 1, int key = -1) {
+            base.Damage(value, key);
+            if (base.EnableDamage(key)) {
+                Sound.SoundList.EnemyHitSE();
                 if (colorEffectCoroutine == null && this.gameObject != null && this.gameObject.activeInHierarchy == true)
                     colorEffectCoroutine = StartCoroutine(DamageColorEffect());
             }
         }
         public override void Defeat() {
-            ADX2LEManager.Instance.PlaySE("Destroy2");
-            GameManager.ScoreAddition(this.score, this.transform.TransformPoint(this.offSet), this.ignoreScoreLimit);
+            Sound.SoundList.EnemyDestroySE();
             InstantiateDestroyEffect();
-            InstantiateDropItem();
+            InstantiateDropItemAndScore();
+            ResetColorAll();
             base.Defeat();
         }
+        protected void InstantiateDropItemAndScore() {
+            GameManager.ScoreAddition(this.score, this.transform.TransformPoint(this.offSet), this.ignoreScoreLimit, this.ignoreScoreCombo);
+            InstantiateDropItem();
+        }
         public void CollisionWithPlayer() {
-            InstantiateDestroyEffect();
             if (!tough) {
-                //SoundManager.enemyDestroySESource.Play();
-                if (base.takeNoDamage) return;
-                ADX2LEManager.Instance.PlaySE("Destroy2");
-                //Destroy(this.gameObject);
+                if (!base.EnableDamage(-1)) return;
+                Sound.SoundList.EnemyDestroySE();
+                InstantiateDestroyEffect();
                 base.Defeat();
             }
         }
-
-        private void InstantiateDestroyEffect(Vector3 relativePos) {
+        protected void InstantiateDestroyEffect(Vector3 relativePos, Vector3 localScale) {
             if (eDestroyEffect != null)
-                Instantiate(eDestroyEffect, this.transform.TransformPoint(relativePos), Quaternion.identity).transform.localScale = this.effectScale * this.defaultEffectScale;
+                Instantiate(eDestroyEffect, this.transform.TransformPoint(relativePos), Quaternion.identity).transform.localScale = localScale;
         }
-        private void InstantiateDestroyEffect() {
-            InstantiateDestroyEffect(this.offSet);
+        protected void InstantiateDestroyEffect() {
+            InstantiateDestroyEffect(this.offSet, this.effectScale * this.defaultEffectScale);
         }
+
         private void InstantiateDropItem() {
             switch (this.selectType) {
                 case ObjectSelectType.Object:
@@ -148,15 +155,23 @@ namespace HyperNova.Entity {
                     break;
             }
         }
+        private void InitColorChanger() {
+            this.colorChangers = new ColorChanger[this.bodysRenderer.Length];
+            for (int i = 0; i < this.colorChangers.Length; i++)
+                this.colorChangers[i] = new ColorChanger(this.bodysRenderer[i].material);
+        }
+        private void ResetColorAll() {
+            if (this.colorChangers == null) return;
+            foreach (var c in this.colorChangers)
+                c.ResetColor();
+        }
         private IEnumerator DamageColorEffect() {
-            if (bodysRenderer == null) yield break;
-            ColorChanger[] colorChangers = new ColorChanger[this.bodysRenderer.Length];
-            for (int i = 0; i < colorChangers.Length; i++)
-                colorChangers[i] = new ColorChanger(this.bodysRenderer[i].material);
-            Color _damageColor = this.damageColor;
-            foreach (var cc in colorChangers) cc.SetColor(_damageColor);
-            yield return new WaitForSeconds(0.1f);
-            foreach (var cc in colorChangers) cc.ResetColor();
+            if (bodysRenderer != null) {
+                Color _damageColor = this.damageColor;
+                foreach (var cc in this.colorChangers) cc.SetColor(_damageColor);
+                yield return new WaitForSeconds(0.1f);
+            }
+            ResetColorAll();
             colorEffectCoroutine = null;
         }
     }
